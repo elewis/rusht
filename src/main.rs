076381078602f79d9d64437dfd3bb42789692a09
Ethan Lewis <3thanlewis@gmail.com>
2015-03-08 15:27:21 -0700

    Added cd and pwd builtins

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ fn main() {
 }
 
 pub mod shell {
+    use std::os;
     use std::old_io;
     use std::process;
     use std::vec::Vec;
+    use std::old_path;
 
     use parse;
 
@@ -36,8 +38,10 @@ pub mod shell {
             Shell {
                 prompt: String::from_str("rusht$ "),
                 builtins: vec![
-                    Builtin { name: "quit", desc: "quit the shell",       func: cmd_quit },
-                    Builtin { name: "help", desc: "print a help message", func: cmd_help },
+                    Builtin { name: "quit", desc: "quit the shell",           func: cmd_quit },
+                    Builtin { name: "help", desc: "print a help message",     func: cmd_help },
+                    Builtin { name: "pwd",  desc: "print working directory",  func: cmd_pwd },
+                    Builtin { name: "cd",   desc: "change working directory", func: cmd_cd }
                 ]
             }
         }
@@ -82,6 +86,32 @@ pub mod shell {
                 }
             }
             None
+        }
+    }
+
+    fn cmd_cd(args: Vec<&str>) -> CommandResult {
+        let new_cwd = old_path::Path::new(args[0]);
+        match os::change_dir(&new_cwd) {
+            Ok(_) => {
+                CommandResult::Success(0)
+            },
+            Err(msg) => {
+                println!("{}", msg);
+                CommandResult::Failure(1)
+            }
+        }
+    }
+
+    fn cmd_pwd(args: Vec<&str>) -> CommandResult {
+        match os::getcwd() {
+            Ok(path) => {
+                println!("{}", path.display());
+                CommandResult::Success(0)
+            },
+            Err(msg) => {
+                println!("{}", msg);
+                CommandResult::Failure(1)
+            }
         }
     }
 
